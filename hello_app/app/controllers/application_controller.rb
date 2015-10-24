02b95e84ac66215454fa7439e6abdffddeb315f9
Julie Farlinger <jjfarlinger@gmail.com>
2015-10-24 18:42:20 +0000

    changed hello to Spanish and added goodbye

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello, world!"
+    render text: "&iexcl;hola, mundo!"
+  end
+  def goodbye
+    render text: "goodbye, world!"
   end
 end
